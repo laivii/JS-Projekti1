b333a6f96feba73a666b4a247ffa9ab1f7ee255f
laivii <91199307+laivii@users.noreply.github.com>
2022-10-25 16:40:48 +0300

    fale save

--- js/script.js
+++ js/script.js
@@ -1,58 +1,70 @@
 document.getElementById("submit").addEventListener("click", submitTask);
+document.getElementById("clearAll").addEventListener("click", clearAll);
 
 function submitTask(){
-    var value = document.getElementById("task").value;
+    var value = document.getElementById("taskvalue").value;
 
+    //Checking that the input is correct (not too short and not empty)
     if(checkContent(value) != true){
         return;
     }
 
     let tasks = document.getElementById("tasklist");
 
+    //Creating a new listing (a new "li" element for a task)
     let newtask = listItem(value);
-    tasks.appendChild(newtask);
+    tasks.appendChild(newtask); //Appending to the excisting "ul" element
 
-    var textarea = document.getElementById("task");
+    //Emptying the textarea for the next task
+    var textarea = document.getElementById("taskvalue");
     textarea.value = "";
 
-    AmountOfTasks();
+    amountOfTasks();
 }
 
 function listItem(x){
-    let määrä = document.getElementsByTagName("li").length;
+    //let määrä = document.getElementsByTagName("li").length;
 
+    //Creating the "li" element
     let newtask = document.createElement("li");
-    newtask.className = "tehtävä";
+    newtask.className = "task";
+    newtask.id = "task";
 
+    //Creating "input" element, defining type, id and class
     let checkbox = document.createElement("input");
         checkbox.type = "checkbox";
-        checkbox.id = "checkbox";
-        checkbox.className = "notdone";
-        checkbox.addEventListener("click", mark);
-    newtask.appendChild(checkbox);
+        checkbox.id = "checkbox"; 
+        checkbox.className = "checkbox";
+        checkbox.className = "custom"; //trying to make a custom checkbox
+        checkbox.addEventListener("click", markTasks); //Adding a listener for functionality
+    newtask.appendChild(checkbox); //Appending to the "li" element
 
+    //Creating a text node (this wont allow using html tags)
     let textNode = document.createTextNode(x);
-    newtask.appendChild(textNode);
+    newtask.appendChild(textNode); //Appending to "li" element
 
+    //Creating a button, defining class and innerHTML (text shown to the user in browser)
     let remove = document.createElement("button");
-        remove.class = "remove";
-        remove.innerHTML = "poista";
-        remove.addEventListener("click", removeTask);
-    newtask.appendChild(remove);
+        remove.className = "remove";
+        remove.innerHTML = "remove";
+        remove.addEventListener("click", removeTask); //Adding a listener for functionality
+    newtask.appendChild(remove); //Appending to the "li" element
 
-    return newtask;
+    return newtask; //Returning the finished "li" element
 }
 
 function checkContent(x){
-    let box = document.getElementById("task");
+    let box = document.getElementById("taskvalue");
 
+    //Chenking that input is not blank
     if(x == ''){
-        box.style.border = "2px dotted red";
+        box.style.border = "2px dotted red"; //Textareas border change if error occurs
 
         alert("Your input cannot be blank!");
         return false;
     }
 
+    //Checking that input is not too short
     if(x.length <= 3){
         box.style.border = "2px dotted red";
 
@@ -60,34 +72,75 @@ function checkContent(x){
         return false;
     }
 
+    //This one return the border to it's original form after error when valid input is submitted
+    //Operates every round when input is valid, but is form the reason stated over this.
     box.style.border = "1px solid grey";
     box.style.borderRadius = "2px"
     return true;
 }
 
-function mark(){
-    let state = this.checked;
+function markTasks(){
+    let state = this.checked; //This one checks the state of the task (done=true or undone=false)
     let task = this.parentElement;
 
+    itemsLeft();
+
+    //If task is being marked undone, this makes the listing take its original form.
     if( state == false){
             task.style.color = "black";
             task.style.textDecoration = "none";
         return;
     }
 
+    //These apply when the task is being marked as done
     task.style.color = "lightgrey";
     task.style.textDecoration = "line-through";
 }
 
 function removeTask(){
+    //Removing task ("li" elements) from the "ul" element
     let removed = this.parentElement;
     removed.remove();
 
-    AmountOfTasks();
-    tasksDone();
-    tasksNotDone();
+    amountOfTasks();
 }
 
 function clearAll(){
+    //The amount of "li" elements
+    let amount = document.getElementsByClassName("task").length;
 
+    //This one goes through every "li" element and removes them
+    for(var i = 0; i < amount; i++){
+        let removed = document.getElementById("task");
+        removed.remove();
+    }
+
+    amountOfTasks();
+}
+
+function amountOfTasks(){
+    //Calculating the amount of tasks ("li" elements)
+    let tasks = document.getElementsByClassName("task").length;
+    let place = document.getElementById("amount");
+
+    place.innerHTML = tasks + " Items left";
+}
+
+function itemsLeft(){
+    var tasks = document.getElementsByClassName("checkbox").length;
+    var done = 0; //Always starting from zero so the count is accurate even when marking tasks undone.
+
+    //Checking the array of "li" elements for their state (done/notdone).
+    for(let i = 0; i < tasks; i++){
+        var state = document.getElementsByClassName("checkbox")[i].checked;
+
+        //Increasing the "done" value with one every time a task is marked done
+        if(state == true){
+            done++;
+        }
+    }
+
+    //Updating the Items left calculator to show accordingly
+    let place = document.getElementById("amount");
+    place.innerHTML = (tasks-done) + " Items left";
 }
